790bb0cf41baba9ce70f34cc35ab9d88d77dcc47
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2023-10-10 11:43:05 +0800

    feat: update playlist

--- packages/music-download/src/playlist.js
+++ packages/music-download/src/playlist.js
@@ -2,9 +2,12 @@
  * @@Author: zhangyunpeng@sensorsdata.cn
  * @@Description: 
  * @Date: 2023-10-08 15:28:37
- * @LastEditTime: 2023-10-08 16:47:38
+ * @LastEditTime: 2023-10-10 11:42:45
  */
 
 module.exports = {
-  id: 8787091946,
+  // SQL注入喜欢的音乐
+  id: 483058795,
+  // VIP歌单
+  // id: 8787091946,
 }
